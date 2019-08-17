670f1ccd4aae5f96917656d553f65626b5242b37
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-08-17 16:31:38 -0300

    9 Questao da lista 1 completa

---
+++ lista01ex09.py
@@ -0,0 +1,10 @@
+SeuNome = str (input ("Qual o seu nome? "))
+SeuNascimento = int(input("Em que ano voce nasceu? "))
+Idade = 2019 - SeuNascimento
+if Idade == 0:
+    print("Voce ainda nao fez aniversario")
+    
+else :
+    print(SeuNome, "Voce tem a idade de:", Idade, "Anos")    
+
+# Bom Ricardo aqui como vc ainda n trabalhou "if" e "else" com a gente eu me recordo um pouco quando eu vi python, quis trabalhar aqui pq se o ano de nascimento fosse igual a 2019 apareceria a mensagem vc tem 0 anos, e ficaria um pouco estranho, se tiver algum problema peço que me fale ou ate mesmo dê uma pontuação negativa na nota da questão!
