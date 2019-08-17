dbcaef1cdbaa9fe3739078e5638d3adb405a24c2
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-08-17 14:04:25 -0300

    4 questao exercicio 1 resolvida

---
+++ lista01ex04.py
@@ -0,0 +1,14 @@
+LarguraComodo1 = float(input("Qual a largura do comodo 1? "))
+ComprimentoComodo1 = float(input("Qual o comprimento do comodo 1? "))
+LarguraComodo2 = float(input("Qual a largura do comodo 2? "))
+ComprimentoComodo2 = float(input("Qual o comprimento do comodo 2? "))
+LarguraComodo3 = float(input("Qual a largura do comodo 3? "))
+ComprimentoComodo3 = float(input("Qual o comprimento do comodo 3? "))
+LarguraComodo4 = float(input("Qual a largura do comodo 4? "))
+ComprimentoComodo4 = float(input("Qual o comprimento do comodo 4? "))
+AreaComdo1 = LarguraComodo1*ComprimentoComodo1
+AreaComdo2 = LarguraComodo2*ComprimentoComodo2
+AreaComdo3 = LarguraComodo3*ComprimentoComodo3
+AreaComdo4 = LarguraComodo4*ComprimentoComodo4
+AreaTotalDaCasa = AreaComdo1+AreaComdo2+AreaComdo3+AreaComdo4
+print ("A area total do comodo 1 sera de:", AreaComdo1,"M2",",", "A area total do comodo 2 sera de:", AreaComdo2,"M2",",", "A area total do comodo 3 sera de:", AreaComdo3,"M2",",", "A area total do comodo 4 sera de:", AreaComdo4,"M2",",", "E a Area Total da casa sera de:", AreaTotalDaCasa,"M2" )
