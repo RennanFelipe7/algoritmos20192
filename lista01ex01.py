cf43912170e1b01fc6e284c834aa662f2ea3f6a5
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-08-17 09:01:43 -0300

    1 Questao da lista 1 completa

---
+++ lista01ex01.py
@@ -0,0 +1,10 @@
+ValordoLdeCombustivel = float (input ('Qual o valor do litro de combustivel na sua cidade? '))
+QuandoVCquerAbastecer = int (input ('Quanto vc quer abastecer? '))
+litrosDisponivel = QuandoVCquerAbastecer/ValordoLdeCombustivel
+print('Voce poderar abastecer', litrosDisponivel, 'litros')
+#Professor Ricardo na linha 2 como na questao vc deu o exemplo de 50 reais eu nao coloquei o valor
+# "float" e sim o valor Inteiro...
+# tambem professor se a gente colocar os valores 1.75 e 66 respectivamente irar dar um valor
+# quebrado porem o senhor n falou nada a respeito n quiz mexer mais no codigo e mostrar o valor 
+# Aredondado... 
+
