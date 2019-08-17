c6d1680e3a7ab92437436f9c1bd3ee61b50eacf8
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-08-17 14:39:53 -0300

    5 questao da lista 1 completa

---
+++ lista01ex05.py
@@ -0,0 +1,4 @@
+CotacaoDoDolar = float(input("Qual o valor da cotacao do doral? "))
+ValorDoRealAConverter = float(input("Quanto em reais vc deseja converter para dolar? "))
+ConversaoDeRealParaDolar = ValorDoRealAConverter/CotacaoDoDolar
+print("Seu valor de:", ValorDoRealAConverter, "reais", "vai valer", ConversaoDeRealParaDolar, "dolar")
