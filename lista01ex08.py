c53f72b303ce2bb385fcb22bb999e24eac44a16d
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-08-19 13:36:21 -0300

    8 Questao da lista 1 completa

---
+++ lista01ex08.py
@@ -0,0 +1,6 @@
+Numero = int(input("Qual o seu numero? "))
+Sucessor1 = Numero + 1
+Sucessor2 = Numero + 2
+Antecessor1 = Numero - 1
+Antecessor2 = Numero - 2
+print(Antecessor2,Antecessor1,Numero,Sucessor1,Sucessor2)
