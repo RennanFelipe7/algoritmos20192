a9d61f162e90264b6e8214efc6c9716a9746203e
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-08-26 19:30:07 -0300

    Questão 1 da lista 2

---
+++ Lista 2/lista02ex01.py
@@ -0,0 +1,9 @@
+Letra = str(input("Qual seu gênero? "))
+if Letra == "F" or Letra =="f":
+    print("Seu gênero é Feminino")
+elif Letra == "M" or Letra == "m":
+    print("Seu gênero é Masculino")
+else :
+    print("Sexo invalido")
+
+# Professor Ricardo aqui eu quis tratar como o usuario poderia digitar uma letra maiuscula ou minuscula para a variavel Letra.
---
+++ Lista 2/lista02ex02.py
@@ -0,0 +1,8 @@
+Letra = str(input("Digite uma letra, lembrando que é em minusculo: "))
+if Letra == 'a' or Letra == 'e' or Letra =='i' or Letra == 'o' or Letra== 'u':
+    print("Sua letra é uma vogal")
+else:
+    print("Sua letra é uma consoante")
+
+
+# Já nessa questão tô levando em consideração que o usuario ira digitar uma letra minuscula que é o que pede no anunciado da linha 1
