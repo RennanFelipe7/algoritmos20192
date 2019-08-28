d530894ea9be6d0c7d99934f052bf61af5aceec0
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-08-28 14:22:57 -0300

    Questão 9 da lista 2

---
+++ Lista 2/lista02ex09.py
@@ -0,0 +1,17 @@
+NumeroDoDiaDaSemana = int(input("Qual dia da semana voce quer verificar que seu numero corresponde? "))
+if NumeroDoDiaDaSemana == 1:
+    print("Seu numero equivale a Domingo")
+elif NumeroDoDiaDaSemana == 2:
+    print("Seu numero equivale a Segunda Feira")
+elif NumeroDoDiaDaSemana == 3:
+    print("Seu numero equivale a Terça Feira")
+elif NumeroDoDiaDaSemana == 4:
+    print("Seu numero equivale a Quarta Feira")
+elif NumeroDoDiaDaSemana == 5:
+    print("Seu numero equivale a Quinta Feira")
+elif NumeroDoDiaDaSemana == 6:
+    print("Seu numero equivale a Sexta Feira")
+elif NumeroDoDiaDaSemana == 7:
+    print("Seu numero equivale a Sabado")
+else:
+    print("Valor Invalido")
