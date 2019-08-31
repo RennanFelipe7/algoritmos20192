db267210865ecc482d06a15bc48a922563186d01
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-08-31 16:44:57 -0300

    Questão 12 da lista 2

--- Lista 2/lista02ex12.py
+++ Lista 2/lista02ex12.py
@@ -1,19 +1,45 @@
 Numero = int(input("Digite um numero menor que 1000 (Mil) e maior que 0 (Zero) "))
-if Numero > 99 and Numero < 1000:
+if Numero <= 0:
+    print("Numero Invalido")
+elif Numero > 99 and Numero < 1000:
     Unidade = (Numero % 10)
     Numero = (Numero-Unidade)/10
     Dezena = int(Numero % 10)
     Numero = (Numero-Dezena)/10
     Centena = int(Numero)
-    print("Seu numero tem:",Centena,"Centenas",",",Dezena,"Dezenas","e",Unidade,"Unidades")
+    if Centena == 1 and Dezena == 1 and Unidade == 1:
+        print("Seu numero tem:",Centena,"Centena",",",Dezena,"Dezena","e",Unidade,"Unidade")
+    elif Centena == 1 and Dezena == 1 and Unidade != 1:
+        print("Seu numero tem:",Centena,"Centena",",",Dezena,"Dezena","e",Unidade,"Unidades")
+    elif Centena == 1 and Dezena != 1 and Unidade != 1:
+        print("Seu numero tem:",Centena,"Centena",",",Dezena,"Dezenas","e",Unidade,"Unidades")
+    elif Centena != 1 and Dezena != 1 and Unidade == 1:
+        print("Seu numero tem:",Centena,"Centenas",",",Dezena,"Dezenas","e",Unidade,"Unidade")
+    elif Centena == 1 and Dezena != 1 and Unidade == 1:
+        print("Seu numero tem:",Centena,"Centena",",",Dezena,"Dezenas","e",Unidade,"Unidade")
+    elif Centena != 1 and Dezena == 1 and Unidade !=1:
+        print("Seu numero tem:",Centena,"Centenas",",",Dezena,"Dezena","e",Unidade,"Unidades")
+    elif Centena != 1 and Dezena == 1 and Unidade == 1:
+        print("Seu numero tem:",Centena,"Centenas",",",Dezena,"Dezena","e",Unidade,"Unidade")
+    else:
+        print("Seu numero tem:",Centena,"Centenas",",",Dezena,"Dezenas","e",Unidade,"Unidades")
 elif Numero > 9 and Numero < 100:
     Unidade = (Numero % 10)
     Numero = (Numero-Unidade)/10
     Dezena = int(Numero % 10)
-    print("Seu numero tem:",Dezena,"Dezenas",",","e",Unidade,"Unidades")
+    if Dezena == 1 and Unidade == 1 :
+        print("Seu numero tem:",Dezena,"Dezena",",","e",Unidade,"Unidade")
+    elif Dezena == 1 and Unidade != 1:
+        print("Seu numero tem:",Dezena,"Dezena",",","e",Unidade,"Unidades")
+    elif Dezena != 1 and Unidade == 1 :
+        print("Seu numero tem:",Dezena,"Dezenas",",","e",Unidade,"Unidade")
+    else:
+        print("Seu numero tem:",Dezena,"Dezenas",",","e",Unidade,"Unidades")    
 elif Numero < 9:
-    Unidade = (Numero%10)
-    print("Seu numero tem:",Unidade,"Unidades")
-    
-
-
+    Unidade = (Numero%10)    
+    if Unidade == 1 :
+        print("Seu numero tem:",Unidade,"Unidade")
+    else:
+        print("Seu numero tem:",Unidade,"Unidades")
+else:
+    print("Numero invalido")
