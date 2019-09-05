097d67658e48f2d5bbbd63dd4d03388893974816
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-09-05 16:21:07 -0300

    Questão 4 da lista 3

---
+++ Lista 3/lista03ex04.py
@@ -0,0 +1,14 @@
+print("-----------------------------------------------")
+print ("Primiero modo numero abaixo de numero")
+print("-----------------------------------------------")
+cont = 1
+while cont <= 20:
+    print(cont," ")
+    cont = cont + 1
+print("-----------------------------------------------")
+print ("Segundo Modo Numero na mesma linha do numero")
+print("-----------------------------------------------")
+cont = 1
+while cont <= 20:
+    print(cont,end=" ")
+    cont = cont + 1
