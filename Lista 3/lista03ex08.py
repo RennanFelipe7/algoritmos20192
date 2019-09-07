96813f58fe715cfe9b5ab9c1f0a93447ef2dc46a
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-09-07 14:26:09 -0300

    Questão 8 da lista 3

--- Lista 3/lista03ex08.py
+++ Lista 3/lista03ex08.py
@@ -1,7 +1,8 @@
+print("Digite dois numeros, lembrando que o primeiro numero tem que ser menor que o segundo, a diferença entre os dois numeros tem que ser maior que 1, e esses dois numeros também não pode ser iguais.")
 MenorNumero = int(input("Qual o menor numero do intervalo? "))
 MaiorNumero = int(input("Qual o maior numero do intervalo? "))
 MaiorNumero = MaiorNumero - 1
-while MenorNumero < MaiorNumero:
+while MenorNumero < MaiorNumero:    
     MenorNumero = MenorNumero + 1
     if MenorNumero // 1 == MenorNumero:
         Intervalo = MenorNumero
