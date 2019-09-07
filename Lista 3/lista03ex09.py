3f3d0e8817f64e8f64cde739851a996cb9c05b0e
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-09-07 14:55:39 -0300

    Questão 9 da lista 3

---
+++ Lista 3/lista03ex09.py
@@ -0,0 +1,14 @@
+NumeroAverATabuada = int(input("Qual numero entre 1 e 10 você quer ver sua tabuada? "))
+Multiplicador = 0
+print("Tabuada de",NumeroAverATabuada,":")
+while Multiplicador < 10:   
+    Multiplicador = Multiplicador + 1
+    Resultado = NumeroAverATabuada * Multiplicador
+
+    print(NumeroAverATabuada,"X",Multiplicador, "=",Resultado)
+
+
+
+
+
+
