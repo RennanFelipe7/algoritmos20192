5440eda87464f5d4fb7ec9369dbb174251501658
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-09-08 11:14:54 -0300

    Questão 19 da lista 3

---
+++ Lista 3/lista03ex19.py
@@ -0,0 +1,22 @@
+QuantosItensOClienteEstaLevando = int(input("Quantos itens o cliente esta levando? "))
+cont = 0
+while cont < QuantosItensOClienteEstaLevando:
+    cont = cont + 1
+    Resultado = cont * 1.99
+print("---------------------------------------------------------------------")
+print("Seu cliente ira pagar um total de",Resultado,"reais por",cont,"itens")
+print("---------------------------------------------------------------------")
+print("E a tabela se encontra abaixo")
+print("---------------------------------------------------------------------")
+cont = 0
+while cont < 50:   
+    cont = cont + 1
+    Resultado = cont * 1.99
+    
+    
+    print(cont,"-","R$",Resultado)
+
+
+
+
+# Professor Ricardo nessa questão eu não consegui interpretar bem ela, se ela queria a tabela com os valores respectivos de 1 a 50 ou se a questão queria o valor equivalente ao total de itens que o cliente comprou, então fiz as duas formas. De qulquer forma não deixa de esta errado.
