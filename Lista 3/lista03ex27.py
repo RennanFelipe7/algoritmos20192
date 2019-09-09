26aece0addc38411dc5efbf8514fc98b6cdfcab2
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-09-09 00:12:13 -0300

    Questão 27 da lista 3

---
+++ Lista 3/lista03ex27.py
@@ -0,0 +1,12 @@
+UltimoValor = int(input("Qual o ultimo valor que você quer encontrar ? "))
+cont = 1
+Numerador = 1
+Denominador = 1
+print("Sequencia : ")
+print("1")
+while cont < UltimoValor:
+    Numerador = Numerador
+    Denominador = Denominador + 1
+    
+    print(Numerador,"/",Denominador) 
+    cont = cont + 1
