00818c007c1098de72920837f551d6b592c83bdb
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-09-15 13:22:14 -0300

    Questão 23 da lista 3

---
+++ Lista 3/lista03ex23.py
@@ -0,0 +1,37 @@
+ValorDaDivida = float(input("Qual o valor da divida? "))
+print("Valor da divida     Valor dos juros     Quantidade de parcelas     Valor da parcela")
+print()
+Resultado = ValorDaDivida
+ValorDessaParte = Resultado
+ValorDoJuro = 0
+Parcela = 1
+ValorDaParcela = ValorDessaParte / 1
+print(ValorDessaParte,"                  ",ValorDoJuro,"%","                   ",Parcela,"                       ",ValorDaParcela)
+print()
+Resultado = ValorDaDivida * 0.1
+ValorDessaParte = Resultado + ValorDaDivida
+ValorDoJuro = 10
+Parcela = 3
+ValorDaParcela = ValorDessaParte / 3
+print(ValorDessaParte,"                  ",ValorDoJuro,"%","                  ",Parcela,"                       ",ValorDaParcela)
+print()
+Resultado = ValorDaDivida * 0.15
+ValorDessaParte = Resultado + ValorDaDivida
+ValorDoJuro = 15
+Parcela = 6
+ValorDaParcela = ValorDessaParte / 6
+print(ValorDessaParte,"                  ",ValorDoJuro,"%","                  ",Parcela,"                       ",ValorDaParcela)
+print()
+Resultado = ValorDaDivida * 0.2
+ValorDessaParte = Resultado + ValorDaDivida
+ValorDoJuro = 20
+Parcela = 9
+ValorDaParcela = ValorDessaParte / 9
+print(ValorDessaParte,"                  ",ValorDoJuro,"%","                  ",Parcela,"                       ",ValorDaParcela)
+print()
+Resultado = ValorDaDivida * 0.25
+ValorDessaParte = Resultado + ValorDaDivida
+ValorDoJuro = 25
+Parcela = 12
+ValorDaParcela = ValorDessaParte / 12
+print(ValorDessaParte,"                  ",ValorDoJuro,"%","                  ",Parcela,"                       ",ValorDaParcela)
