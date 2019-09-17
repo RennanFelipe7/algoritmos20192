a815b3d315b335cc77d4635ddb8de00d1a05cc9c
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-09-17 07:10:54 -0300

    Questão 1 da lista 4

---
+++ Lista 4/lista04ex01.py
@@ -0,0 +1,7 @@
+for i in range (2,10):
+    print()
+    print("Tabuada do",i)
+    for j in range (1,11):
+        Resultado = i * j                
+        print()
+        print(i,"*",j,"=",Resultado)    
