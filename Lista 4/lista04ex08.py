4c92c012d2b4dff332dc38daafe9a5290212ea94
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-09-21 10:34:17 -0300

    Questão 8 da lista 4

---
+++ Lista 4/lista04ex08.py
@@ -0,0 +1,19 @@
+UsarOCaixa = str(input("Você quer usar o caixa, Digite S para (sim) e N para (não) ? "))
+print()
+QuantoACompraCustouAoCliente = 0
+TotalDeLucroDoCaixa = 0 
+while UsarOCaixa == "S" or UsarOCaixa == "s":
+    FecharACompraDesseCliente = "S"
+    while FecharACompraDesseCliente == "S" or FecharACompraDesseCliente == "s":        
+        Produto = float(input("Qual o preço do produto? "))
+        print()
+        FecharACompraDesseCliente = str(input("O cliente comprou mais itens? digite S (sim) para continuar e N (não) para finalizar a compra desse cliente "))
+        print()
+        QuantoACompraCustouAoCliente = QuantoACompraCustouAoCliente + Produto        
+    print("A compra custou ao cliente um total de",QuantoACompraCustouAoCliente,"reais")
+    print()
+    UsarOCaixa = str(input("Você quer usar o caixa, Digite S para (sim) e N para (não) ? "))
+    print()
+    TotalDeLucroDoCaixa = TotalDeLucroDoCaixa + QuantoACompraCustouAoCliente
+    QuantoACompraCustouAoCliente = 0
+print("O caixa apurou no dia um total de",TotalDeLucroDoCaixa,"reais")
