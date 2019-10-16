501f3fa83a58884cc23194f9d5c373a778df4160
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-10-16 06:31:58 -0300

    Questão 1 da lista 5

---
+++ Lista 5/lista05ex01.py
@@ -0,0 +1,7 @@
+A = [1,0,5,-2,-5,7]
+Soma = A[0] + A[1] + A[5]
+print("A soma dos valores da lista da posição A[0], A[1], A[5] é",Soma)
+A[4] = 100
+print("Valores da lista, cada valor em linhas diferentes")
+for r in range(0,6,1):
+    print(A[r])
