df6f5da9e8f35a76ba278eb7d55c25c8fd5f43c3
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-11-10 13:53:40 -0200

    Questão 7 da lista 6

---
+++ Lista 6/lista06ex07.py
@@ -0,0 +1,22 @@
+OrdemDaMatriz = int(input("Ordem da matriz? "))
+Matriz = []
+AuxDaDiagonalSecundaria = OrdemDaMatriz - 1
+print()
+SomaElementosDaDiagonalSecundaria = 0
+for i in range(OrdemDaMatriz):
+    Matriz.append([0]* OrdemDaMatriz)
+for c in range(OrdemDaMatriz):
+    for d in range(OrdemDaMatriz):
+        Matriz[c][d] = int(input("Qual o elemento da segunda matriz [" + str (c+1) +"][" + str (d+1) + "] ? "))
+for x in range(OrdemDaMatriz):
+    SomaElementosDaDiagonalSecundaria = SomaElementosDaDiagonalSecundaria + Matriz[x][AuxDaDiagonalSecundaria]
+    AuxDaDiagonalSecundaria = AuxDaDiagonalSecundaria - 1 
+print()
+print("Sua matriz formada é")
+print()
+for a in range(OrdemDaMatriz):
+    for b in range(OrdemDaMatriz):
+        print(Matriz[a][b], end=" ")
+    print()
+print()
+print("A soma de seus elementos da diagonal secundaria é:",SomaElementosDaDiagonalSecundaria) 
