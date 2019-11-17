db2afda594af8f47f68e2875a491a295f6bf7ab1
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-11-17 11:06:43 -0200

    Questão 3 da lista 7

---
+++ Lista 7/lista07ex03.py
@@ -0,0 +1,18 @@
+TamanhoDoVetor = int(input("Tamanho do vetor ? "))
+print()
+Vetor = [""] * TamanhoDoVetor
+VetorInverso = [""] * TamanhoDoVetor
+def CriaVetor(XVetor):
+    for i in range(TamanhoDoVetor):
+        Vetor[i] = str(input("Elemento do vetor posição " + str (i+1) + " ? "))
+
+def InverterVetor (VetorOriginal,VetorInversoFuncao,Tamanho):
+    UltimoIndice = Tamanho - 1   
+    for a in range(Tamanho):
+        VetorInversoFuncao[UltimoIndice] = VetorOriginal[a]
+        UltimoIndice = UltimoIndice - 1
+    print()                
+    print("Seu vetor na ordem inversa é",VetorInversoFuncao)
+    
+CriaVetor(Vetor)
+InverterVetor(Vetor,VetorInverso,TamanhoDoVetor)
