7f567f5f93533ce52e351bf83205f1d3242cf1dc
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-11-17 16:15:53 -0200

    Questão 3 da lista 7

--- Lista 7/lista07ex03.py
+++ Lista 7/lista07ex03.py
@@ -5,7 +5,6 @@ VetorInverso = [""] * TamanhoDoVetor
 def CriaVetor(XVetor,Tamanho):
     for i in range(Tamanho):
         XVetor[i] = str(input("Elemento do vetor posição " + str (i+1) + " ? "))
-
 def InverterVetor (VetorOriginal,VetorInversoFuncao,Tamanho):
     UltimoIndice = Tamanho - 1   
     for a in range(Tamanho):
