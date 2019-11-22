0244baf2c71ed6817a057cc45f52b24c8ccd92e3
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-11-22 13:49:56 -0200

    Questão 1 da lista 7

--- Lista 7/lista07ex01.py
+++ Lista 7/lista07ex01.py
@@ -1,23 +1,23 @@
-Vetor = []
-def CriaVetorEImprimePareImpar (FuncaoVetor):    
-    TamanhoDoVetor = int(input("Quantidade de elementos do seu vetor ? "))
-    print()
-    FuncaoVetor = [0] * TamanhoDoVetor
-    for i in range(TamanhoDoVetor):
-        FuncaoVetor[i] = int(input("Elemento " + str(i+1) + " do vetor ? "))
-    print()
-    print("Seu vetor formado foi :",FuncaoVetor)
-    print()
+TamanhoDoVetor = int(input("Qual o tamanho do vetor ? "))
+Vetor = [0] * TamanhoDoVetor
+print()
+def InsereElementosNoVetor (XVetor,Tamanho):    
+    for i in range(Tamanho):
+        XVetor[i] = int(input("Elemento " + str(i+1) + " do vetor ? "))
+def ImprimeParesEImpares(Vet,XTamanho):
     print("Seus elementos pares do vetor são:")   
     print() 
-    for a in range(TamanhoDoVetor):
-        if FuncaoVetor[a] % 2 == 0: 
-            print(FuncaoVetor[a],end="  ")
+    for a in range(XTamanho):
+        if Vet[a] % 2 == 0: 
+            print(Vet[a],end="  ")
     print()
     print()
     print("Seus elementos ímpares do vetor são:")
     print()
-    for a in range(TamanhoDoVetor):
-        if FuncaoVetor[a] % 2 != 0:
-            print(FuncaoVetor[a],end="  ")
-CriaVetorEImprimePareImpar(Vetor)
+    for b in range(XTamanho):
+        if Vet[b] % 2 != 0:
+            print(Vet[b],end="  ")
+
+InsereElementosNoVetor(Vetor,TamanhoDoVetor)
+print()
+ImprimeParesEImpares(Vetor,TamanhoDoVetor)
