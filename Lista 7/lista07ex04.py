a4a2a83aabf8fd3fe33c9dad88ee123c5bfb0527
rennanfelipe7 <https://github.com/RennanFelipe7/algoritmos20192.git>
2019-11-22 13:11:10 -0200

    Questão 4 da lista 7

--- Lista 7/lista07ex04.py
+++ Lista 7/lista07ex04.py
@@ -4,16 +4,19 @@ Vetor = [0] * TamanhoDoVetor
 def CriaVetor(VetorNumeros,Tamanho):
     for i in range(Tamanho):
         VetorNumeros[i] = int(input("Elemento do vetor posição " + str (i+1) + " ? "))
-    print()
 def NumerosEstaoEmOrdemCrescente(VetorOriginal,Tamanho):
-    Resultado = True
-    for a in range(0,Tamanho - 1):  # 0 1 2 p 3
-        for b in range(a+1,Tamanho):
-            if VetorOriginal[a] > VetorOriginal[b]:
-                Resultado = False
-    if Resultado == False:
-        print("Os elementos do seu vetor não se encontra em ordem crescente")
-    else:
-        print("Os elementos do seu vetor encontra-se em ordem crescente")
+    for a in range(Tamanho - 1): 
+        if VetorOriginal[a] > VetorOriginal[a+1]:
+            return  False
+    return True
 CriaVetor(Vetor,TamanhoDoVetor)
 NumerosEstaoEmOrdemCrescente(Vetor,TamanhoDoVetor)
+print()
+print("Seu vetor formado foi")
+print()
+print(Vetor)
+print()
+if NumerosEstaoEmOrdemCrescente(Vetor,TamanhoDoVetor):
+    print("Seu vetor está em ordem crescente")
+else:
+    print("Seu vetor não está em ordem crescente")
